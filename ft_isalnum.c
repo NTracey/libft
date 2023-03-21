1acdd1dc36280fb79213070b08480c7913945dba
Tracey Ngo <traceyngo8@gmail.com>
2023-03-21 19:14:25 +1030

    added ft_isalnum.c

---
+++ ft_isalnum.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trngo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 20:48:17 by trngo             #+#    #+#             */
+/*   Updated: 2023/03/21 18:53:39 by trngo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isalnum(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+    printf("Result for 1: %d\n", ft_isalnum('1'));
+	printf("Result for a: %d\n", ft_isalnum('a'));
+	printf("Result for !: %d\n", ft_isalnum('!'));
+	return (0);
+}
+*/
