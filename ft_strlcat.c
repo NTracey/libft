d7c40442b39b93851149772bf9dc52a46f30c2ba
Tracey Ngo <traceyngo8@gmail.com>
2023-04-13 21:55:38 +0930

    need to review and change strlcat, strnstr, and atoi

---
+++ ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trngo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 18:20:39 by trngo             #+#    #+#             */
+/*   Updated: 2023/04/13 20:48:58 by trngo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dest_len;
+	size_t	src_len;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
+	if (size < dest_len)
+		src_len = src_len + size;
+	else
+		src_len = src_len + dest_len;
+	i = 0;
+	while (src[i] != '\0' && (dest_len + i + 1) < size)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (src_len);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trngo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 20:28:52 by trngo             #+#    #+#             */
+/*   Updated: 2023/04/13 21:14:28 by trngo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (*little == 0)
+		return ((char *)big);
+	i = 0;
+	while (big[i] != '\0' && i < len)
+	{
+    	j = 0;
+		while (big[i + j] == little[j] && i + j < len)
+		{
+			j++;
+			if (little[j] == '\0')
+				return ((char *)&big[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
